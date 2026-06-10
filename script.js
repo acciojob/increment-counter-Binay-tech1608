69478f8d88a3e28c676f02419a7d189d21bd64f6
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-06-11 01:46:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let counter = document.getElementById("counter");
+let count = 0 ;
+let mybtn = document.getElementById("btn");
+
+mybtn.addEventListener("click", ()=>{
+	count++;
+	counter.textContent = count;
+})
